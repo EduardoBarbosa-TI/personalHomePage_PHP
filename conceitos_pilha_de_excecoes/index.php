376eea7088f2994bd8fda73f16b7cd4b9c0840e0
eduardobarbosa-ti <eduardobb@rede.ulbra.br>
2022-11-05 16:56:03 -0300

    modified files

--- conceitos_pilha_de_excecoes/index.php
+++ conceitos_pilha_de_excecoes/index.php
@@ -27,21 +27,21 @@ f2(
 
 //Tratamento de exeções
 
-function divisao($dividendo, $divisor){
-    try{
-        if($divisor == 0){
-            throw new RangeException("O número não pode ser dividido por zero");
-        }
-        $resultado = $dividendo / $divisor;
+// function divisao($dividendo, $divisor){
+//     try{
+//         if($divisor == 0){
+//             throw new RangeException("O número não pode ser dividido por zero");
+//         }
+//         $resultado = $dividendo / $divisor;
 
-        echo "O resultado é:" . $resultado;
-    } catch (Exception $e){
-        echo $e -> getMessage();
-    }
-    finally{
-        echo "<br> Tratando exceções";
-    }
-}
+//         echo "O resultado é:" . $resultado;
+//     } catch (Exception $e){
+//         echo $e -> getMessage();
+//     }
+//     finally{
+//         echo "<br> Tratando exceções";
+//     }
+// }
 
 divisao(10,0);
 
